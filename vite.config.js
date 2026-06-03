5b89ab4949143724b43e090e723d1435e9d80e43
Nocturnal Wisp <rogersteve97@gmail.com>
2026-06-03 12:22:31 -0600

    Updated site

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  // This is a user/organization site (NocturnalWisp.github.io),
+  // so use a root base. For project pages use '/repo-name/'.
+  base: '/', 
+  build: {
+    outDir: 'docs'
+  }
+})
